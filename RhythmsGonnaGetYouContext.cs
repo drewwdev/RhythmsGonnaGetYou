35b2427d1baaffe41e378568246ba713127188be
Drew Wilson <jankyy93@gmail.com>
2021-08-13 12:09:55 -0400

    Classes set up

---
+++ RhythmsGonnaGetYouContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace RhythmsGonnaGetYou
+{
+    public class RhythmsGonnaGetYouContext : DbContext
+    {
+        public DbSet<Album> Album { get; set; }
+        public DbSet<Band> Band { get; set; }
+        public DbSet<Song> Song { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("server=localhost;database=SuncoastMovies");
+            var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+            optionsBuilder.UseLoggerFactory(loggerFactory);
+        }
+    }
+}
---
+++ Song.cs
@@ -0,0 +1,14 @@
+
+namespace RhythmsGonnaGetYou
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public int TrackNumber { get; set; }
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public int AlbumId { get; set; }
+
+        public Album Album { get; set; }
+    }
+}
